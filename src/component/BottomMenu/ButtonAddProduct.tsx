33406fcfdff8f540253699bed7e659fa02dcbc21
Magda Jarzyna <magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.com>
2022-03-02 14:05:30 +0100

    add modal to button add product in bottom menu

--- src/component/BottomMenu/ButtonAddProduct.tsx
+++ src/component/BottomMenu/ButtonAddProduct.tsx
@@ -1,22 +1,23 @@
-import {useState} from 'react';
-
-
+import React, { Component, FunctionComponent, useState } from 'react';
+import { render } from 'react-dom';
+import { Modal } from '../Modal';
+import { useModal } from '../UseModal';
 const ButtonAddProduct = () => {
-
-    const [showModal, setShowModal] = useState(false);
+    const { isShown, toggle } = useModal();
+    const content = <React.Fragment>Hey, I'm a model.</React.Fragment>;
     return(
         <>
             <button
                 type="button"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModalCenteredScrollable"
-                onClick={() => setShowModal(true)}
+                onClick={toggle}
                 className="inline-block px-6 py-2.5 bg-purple text-white font-medium text-xs font-extra-bold leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >Dodaj product
             </button>
-
+            <Modal isShown={isShown} hide={toggle} modalContent={content} modalHeaderText={"string"} />
         </>
     )
 
 }
-export  default ButtonAddProduct;
+export default ButtonAddProduct;
---
+++ src/component/Modal.tsx
@@ -0,0 +1,45 @@
+ import React, { FunctionComponent, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+    Wrapper,
+    Header,
+    StyledModal,
+    HeaderText,
+    CloseButton,
+    Content,
+    Backdrop,
+} from './modalStyle';
+
+export interface ModalProps {
+    isShown: boolean;
+    hide: () => void;
+    modalContent: JSX.Element;
+    modalHeaderText: string | null;
+}
+
+export const Modal: FunctionComponent<ModalProps> = ({
+                                                         isShown,
+                                                         hide,
+                                                         modalContent,
+                                                         modalHeaderText,
+                                                     }) => {
+    const modal = (
+        <React.Fragment>
+            <Backdrop />
+            <Wrapper tabIndex={-1} role="dialog">
+                <StyledModal>
+                    <Header>
+                        <HeaderText>{modalHeaderText}</HeaderText>
+                        <CloseButton onClick={hide}>X</CloseButton>
+                    </Header>
+                    <Content>{modalContent}</Content>
+                </StyledModal>
+            </Wrapper>
+        </React.Fragment>
+    );
+
+    return isShown ? ReactDOM.createPortal(modal, document.body) : null;
+};
+
+
