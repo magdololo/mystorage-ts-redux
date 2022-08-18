2271ae311fc9273e0467752f14c336071bdfc01e
Magda Jarzyna <magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.com>
2022-08-18 13:22:45 +0200

    home page layout

--- src/features/api/Home.tsx
+++ src/features/api/Home.tsx
@@ -7,7 +7,7 @@ const Home = ()=>{
          <>
              <header className="">
                  <nav
-                     className="navbar max-w-7xl relative flex items-center w-full justify-between mx-auto h-40 fixed bg-white w-full z-50 top-0 left-0 font-courgette text-purple-800 text-2lg ">
+                     className="navbar max-w-7xl w-full mx-auto h-40 fixed bg-white relative flex items-center justify-between z-50 top-0 left-0 font-courgette text-purple-800 text-lg lg:text-2lg ">
                  <div className="px-6">
                      <button
                          className="navbar-toggler border-0 py-3 px-2 md:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out"
@@ -37,7 +37,7 @@ const Home = ()=>{
                          <ul className="navbar-nav mr-auto flex flex-col md:flex-row">
                              <li className="nav-item">
                                  <a
-                                     className="nav-link block p-2 text-gray-400 text-2lg hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                                     className="nav-link block p-2 text-gray-400 text-2lg hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out  "
                                      href="#!"
                                      data-mdb-ripple="true"
                                      data-mdb-ripple-color="light"
@@ -58,43 +58,72 @@ const Home = ()=>{
                      className="inline-block px-6 py-2.5 bg-purple-800 text-white text-lg font-bold leading-tight rounded-2xl shadow-md hover:bg-purple-900 hover:shadow-lg focus:bg-purple-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
                      data-mdb-ripple="true"
                      data-mdb-ripple-color="light"
-                 >
-                     Zarejestruj się
+                 ><Link to="/register">Zarejestruj się</Link>
                  </button>
 
              </div>
                  </nav>
             <div className="relative flex items-center w-full  max-w-6xl mx-auto pt-12 font-noto-sans">
-                 <div className="px-6 text-left w-3/5 mr-5 max-w-2xl ">
-                     <h2 className="font-bold  text-font-home text-7xl mb-4 leading-tight">Twoje produkty zawsze pod ręką</h2>
-                     <h4 className=" text-gray-light text-xl mb-6 leading-loose">Zdobądź łatwy dostęp do produktów w swoim domu aby lepiej planować zakupy, posiłki i czas.</h4>
+                 <div className="px-12 text-left w-3/5 mr-5 max-w-2xl lg:px-6">
+                     <h2 className="font-bold  text-font-home text-5xl mb-4 leading-tight lg:text-7xl">Twoje produkty zawsze pod ręką</h2>
+                     <h4 className=" text-gray-light text-lg mb-6 leading-loose lg:text-xl lg:leading-10">Zdobądź łatwy dostęp do produktów w swoim domu aby lepiej planować zakupy, posiłki i czas.</h4>
                      <button
                          type="button"
                          className="inline-block px-6 py-3.5 bg-purple-800 text-white text-lg font-bold leading-tight rounded-2xl shadow-md hover:bg-purple-900 hover:shadow-lg focus:bg-purple-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
                          data-mdb-ripple="true"
                          data-mdb-ripple-color="light"
-                     >
-                         Zarejestruj się
+                     ><Link to="/register">Zarejestruj się</Link>
                      </button>
                  </div>
                 <div className="px-6">
-                    <img src="../images/categories_screen.png" className={"max-h-144"}/>
+                    <img src="../images/categories_screen.png" className="max-h-128 lg:max-h-144"/>
 
                 </div>
             </div>
              </header>
-             <section className={"pt-5 max-w-full bg-homeSection mx-auto relative"}>
+             <section className={"pt-24 max-w-full bg-homeSection mx-auto relative shadow-zz pb-2"}>
                  <div className={"flex max-w-7xl w-full  mx-auto relative items-center flex-wrap"}>
                      <div className="px-6 text-left w-2/5 max-w-2xl pl-20">
-                         <img  src="../images/addCategory_screen.png" className={"max-h-144"}/>
+                         <div className="inline-block relative">
+                             <span className="absolute bottom-0 h-1 w-full text-gray-light shadow-zz box-decoration-clone"></span>
+                             <img  src="../images/addProduct_screen.png" className="max-h-128 lg:max-h-144"/>
+                         </div>
                      </div>
                      <div className="px-6 text-left w-1/2  max-w-2xl ml-3">
-                         <h2 className="font-bold  text-font-home text-5xl mb-4 leading-normal">Twórz własne kategorie</h2>
+                         <h2 className="font-bold  text-font-home text-5xl mb-4 leading-normal">Twórz własne kategorie i dodawaj produkty</h2>
                          <h4 className=" text-gray-light text-xl mb-6 leading-loose">Nazwy kategorii układają się alfabetycznie dzięki czemu łatwo je odnajdziesz. Możesz także dodawać własne obrazki i zdjęcia.</h4>
                      </div>
                  </div>
 
              </section>
+             <section className={"pt-24 max-w-full bg-white mx-auto relative shadow-zz"}>
+                 <div className={"flex max-w-7xl w-full  mx-auto relative items-center flex-wrap "}>
+                     <div className="px-6 text-left w-1/2  max-w-2xl ml-3">
+                         <h2 className="font-bold  text-font-home text-5xl mb-4 leading-normal">Latwe edytowanie</h2>
+                         <h4 className=" text-gray-light text-xl mb-6 leading-loose">Szybko dodawaj i usuwaj wybrany produkt. Mozesz zmienic kategorie oraz sprawdziC czy produkt przekroczył date ważności.</h4>
+                     </div>
+                     <div className="px-6 text-left w-2/5 max-w-2xl pl-20   ">
+                         <div className="inline-block relative">
+                             <span className="absolute bottom-0 h-1 w-full text-gray-light shadow-zz box-decoration-clone"></span>
+                            <img  src="../images/easyEdit_screen.png" className="max-h-128 lg:max-h-144"/>
+                         </div>
+                     </div>
+                 </div>
+
+             </section>
+             <section className={"pt-24 max-w-full bg-homeSection mx-auto relative  shadow-zz"}>
+                 <div className={"flex max-w-7xl w-full  mx-auto relative items-center flex-wrap"}>
+                     <div className="px-6 text-left w-2/5 max-w-2xl pl-20">
+                         <img  src="../images/search_screen.png" className={"max-h-144"}/>
+                     </div>
+                     <div className="px-6 text-left w-1/2  max-w-2xl ml-3">
+                         <h2 className="font-bold  text-font-home text-5xl mb-4 leading-normal">Szybkie wyszukiwanie</h2>
+                         <h4 className=" text-gray-light text-xl mb-6 leading-loose">Dzięki wyszukiwarce odnajdziesz wszystkie podobne produkty jak również konkretny produkt.</h4>
+                     </div>
+                 </div>
+
+             </section>
+             <section className={"pt-2 max-w-full bg-homeSection mx-auto relative  shadow-zz"}/>
          </>
     )
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -78,6 +78,9 @@ module.exports = {
       },
       zIndex: {
         "z-100" : "2500"
+      },
+      boxShadow: {
+        "zz" : "3em 3em 5em #1F2937"
       }
 
     },
