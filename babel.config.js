b8124d2fe6f2b6211d838b936f11d51ac1279f29
Magda Jarzyna <magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.com>
2022-10-05 12:43:12 +0200

    fixing warnings

--- babel.config.js
+++ babel.config.js
@@ -1,36 +1,6 @@
-// module.exports = function (api) {
-//     return {
-//         plugins: ['macros'],
-//     }
-// }
 module.exports = function (api) {
-    const presets = [
-        [
-            '@babel/preset-env',
-            {
-                useBuiltIns: 'usage',
-                corejs: { version: 3, proposals: true }
-            }
-        ],
-        '@babel/preset-react',
-        '@babel/preset-flow'
-    ];
-    const plugins = [
-
-        'lodash',
-        ['@babel/plugin-transform-spread', { loose: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        '@babel/plugin-transform-runtime'
-    ];
-
-    /** this is just for minimal working purposes,
-     * for testing larger applications it is
-     * advisable to cache the transpiled modules in
-     * node_modules/.bin/.cache/@babel/register* */
-    api.cache(false);
-
+    api.cache(true);
     return {
-        presets,
-        plugins
-    };
-};
+        plugins: ['macros'],
+    }
+}
