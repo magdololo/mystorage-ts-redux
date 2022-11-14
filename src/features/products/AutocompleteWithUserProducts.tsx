e3877744f6aa24075a146f90c49b10fbef248320
Magda Jarzyna <magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.com>
2022-11-14 15:25:39 +0100

    fixing notification style on mobile

--- src/features/products/AutocompleteWithUserProducts.tsx
+++ src/features/products/AutocompleteWithUserProducts.tsx
@@ -20,6 +20,7 @@ export const AutocompleteWithUserProducts = ({onChange, value, setSelectedProduc
     const { t } = useTranslation();
     const allProducts = useAppSelector(selectAllProducts)
     const [newProductName,setNewProductName] = useState<string | null>(null);
+    console.log(newProductName)
     return (
 
         <Autocomplete
