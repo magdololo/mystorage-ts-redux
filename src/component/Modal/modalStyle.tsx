fca610169a40bb1bd7211408b26d5611bf3ee3bf
Magda Jarzyna <magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.com>
2022-11-14 15:16:42 +0100

    fixing notification style on mobile

--- src/component/Modal/modalStyle.tsx
+++ src/component/Modal/modalStyle.tsx
@@ -54,12 +54,6 @@ export const StyledModal = styled.div`
   }
 `;
 
-export const Header = styled.div`
-  //border-radius: 8px 8px 0 0;
-  //display: flex;
-  //justify-content: space-between;
-  //padding: 0.3rem;
-`;
 
 
 export const HeaderText = styled.div`
